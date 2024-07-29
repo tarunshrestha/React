76effed23b48c6c9960ac797b46d829701c8786f
tarunshrestha <tarunshrestha90@gmail.com>
2024-07-29 19:37:54 +0545

    static

--- todo-project/src/App.jsx
+++ todo-project/src/App.jsx
@@ -3,33 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <br></br>
+        <input type="text" id="item" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <button className='btn'>Add</button>
+    </form>
 
-export default App
+    <h1 className=''>TodoList</h1>
+    <ul className='list'>
+      <li>  
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className='"btn btn-danger'>Delete</button>
+      </li>
+
+      <li>  
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className='"btn btn-danger'>Delete</button>
+      </li>
+    </ul>
+  </>
+)
+}
