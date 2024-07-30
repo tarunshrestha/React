f75487a470919d0f597be9cd9f27c59597ae2b5d
tarunshrestha <tarunshrestha90@gmail.com>
2024-07-30 18:55:27 +0545

    local Storage

--- todo-project/src/App.jsx
+++ todo-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,15 @@ import { TodoList } from './TodoList'
 
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title){
     setTodos(currentTodos => {
