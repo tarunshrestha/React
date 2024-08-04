d5d6f6834c657c4346e5ca510a57d1c3983e3bdf
tarunshrestha <tarunshrestha90@gmail.com>
2024-08-04 19:50:46 +0545

    Components

---
+++ Coursera/ReactBasics/customizing-example/src/components/Nav.js
@@ -0,0 +1,13 @@
+export default function Nav(){
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>Home</li>
+                <li>Article</li>
+                <li>About</li>
+                <li>Contact</li>
+
+            </ul>
+        </nav>
+    )
+}
---
+++ Coursera/ReactBasics/customizing-example/src/components/Promo.js
@@ -0,0 +1,12 @@
+export default function Promo(){
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Don't miss this deal!</h1>
+            </div>
+            <div>
+                <h2>Subscribe to ny newsletter and get all the shop items at 50% off!!</h2>
+            </div>
+        </div>
+    )
+}
