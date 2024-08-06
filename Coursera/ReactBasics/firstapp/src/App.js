c0bc1a01e383eb62beee796281e9ad6a6a44df8a
tarunshrestha <tarunshrestha90@gmail.com>
2024-08-06 16:59:03 +0545

    params

--- Coursera/ReactBasics/firstapp/src/App.js
+++ Coursera/ReactBasics/firstapp/src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
         
-        <Header title="React"/>
-      </header>
-    </div>
-  );
+  //       <Header title="React"/>
+  //     </header>
+  //   </div>
+  // );
+
+  //   return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello there"));
+
+  // The way this works is described in React docs using the following code:
+  // React.createElement(
+  // type,
+  // [props],
+  // [...children]
+  // )
+  return React.createElement(
+   "div",
+    {Header},
+      React.createElement("h1", {Header}, "Hello there")
+   );
+  
 }
 
 function Header({title}){
