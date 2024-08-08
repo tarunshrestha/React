5de6eb6eca3b4975a87eb1b23d57c1788c2f4b37
tarunshrestha <tarunshrestha90@gmail.com>
2024-08-08 18:12:33 +0545

    touch fliped

--- Documentation/tic-tac-toe/src/components/Board.jsx
+++ Documentation/tic-tac-toe/src/components/Board.jsx
@@ -3,11 +3,19 @@ import { useState } from "react"
 
 export default function Board(){
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+
 
     function handleClick(i){
-        const nextSquare = squares.slice()
-        nextSquare[i] = "X"
-        setSquares(nextSquare)
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+          nextSquares[i] = "X";
+        } else {
+          nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+      
     }
 
     return (
