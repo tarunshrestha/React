af769b365bff5eb2c3aaccd958a0d4a88e905163
tarunshrestha <tarunshrestha90@gmail.com>
2024-08-14 17:19:34 +0545

    rework

---
+++ react_router/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <>
+        <a href="/"><li>Home</li></a>
+        <a href="/"><li>About</li></a>
+        <a href="/"><li>Contact</li></a>
+        </>
+    )
+}
