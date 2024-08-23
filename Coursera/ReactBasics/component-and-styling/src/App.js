ed2ccf387358db48fb6288dced9d602cd1984d72
tarunshrestha <tarunshrestha90@gmail.com>
2024-08-23 11:45:19 +0545

    module 1 complete

--- Coursera/ReactBasics/component-and-styling/src/App.js
+++ Coursera/ReactBasics/component-and-styling/src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
+import avatar from './avatar.png'
+import Card from './components/Card';
 
+function Logo(){
+    const userPic = <img src={avatar} alt="logo" />
+    return userPic
+}
 
 function App() {
   const user = {
     name: "Tarun",
-    type: "Main"
+    type: "Main",
   } 
   return (
     <>
       < Header user={user} />
+      <Logo />
       < Sidebar type={user.type} />
       < Main />
+      <Card  h2="First card's h2" h3="First card's h3" />
+      <Card h2="Second card's h2" h3="Second card's h3" />
+      <Card h2="Third card's h2" h3="Third card's h3" />
+
 
     </>
   );
---
+++ Coursera/ReactBasics/component-and-styling/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({h2, h3}) => {
+  return (
+    <div className="card">
+    <h2>{h2}</h2>
+    <h3>{h3}</h3>
+</div>
+  )
+}
+
+export default Card
