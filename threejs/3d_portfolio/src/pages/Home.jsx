abdcd2ee2fb3bce0300ee87047fb4fa5033ea6e8
tarun <tarunshrestha90@gmail.com>
2024-09-05 11:10:33 +0545

    island rendered

--- threejs/3d_portfolio/src/pages/Home.jsx
+++ threejs/3d_portfolio/src/pages/Home.jsx
@@ -10,7 +10,20 @@ import Island from "../models/Island";
 </div> */}
 
 const Home = () => {
-    console.log('Home is rendering'); // Debug line
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0]
+  
+    if(window.innerWidth < 768){
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1]
+    }
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
 
   return (
     <section className='w-full h-screen relative'>
@@ -22,7 +35,10 @@ const Home = () => {
           <SpotLight />
           <hemisphereLight />
           
-          <Island />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation} />
         </Suspense>
       </Canvas>
     </section>
