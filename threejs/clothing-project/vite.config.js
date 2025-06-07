57a4c35369431d280f24a3c4fa75952db756db09
tarunshrestha <tarunshrestha90@gmail.com>
2025-06-07 19:28:50 +0545

    tailwind

--- threejs/clothing-project/vite.config.js
+++ threejs/clothing-project/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [    
     tailwindcss(),
